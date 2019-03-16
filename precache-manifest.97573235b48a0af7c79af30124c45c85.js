d6b25490376a7a935a91e42272d74dec4f8a2894
Oleksandra88 <ok.007@wp.pl>
2019-03-16 14:49:04 +0100

    my firs app in react

---
+++ precache-manifest.97573235b48a0af7c79af30124c45c85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "95c71ef7fe539d8ee6c6",
+    "url": "/static/js/main.cac82bd3.chunk.js"
+  },
+  {
+    "revision": "cc764c95d1f29763e514",
+    "url": "/static/js/2.2c8f914b.chunk.js"
+  },
+  {
+    "revision": "95c71ef7fe539d8ee6c6",
+    "url": "/static/css/main.12035010.chunk.css"
+  },
+  {
+    "revision": "cc764c95d1f29763e514",
+    "url": "/static/css/2.cef8e257.chunk.css"
+  },
+  {
+    "revision": "f91392690a0351c652aeb2ef28fbda76",
+    "url": "/index.html"
+  }
+];
